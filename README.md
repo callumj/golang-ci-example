769f8d3ccd499c649edc01fe7a68de9bc97c0b4c
Callum Jones <callum@callumj.com>
2014-07-16 23:01:17 +0800

    Docs

--- README.md
+++ README.md
@@ -4,4 +4,6 @@ This repo demonstrates a Go application being built in a CI environment without 
 
 `build.sh` sets a temporary `GOPATH` which contains this application, it then performs a go get on this temporary go package which forces dependencies to be resolved. Finally the application is cross compiled to Linux, Darwin (OS X) and Windows and tar gzipped into the builds directory under the correct `VERSION`.
 
+You may wish to see the `build.sh` yourself for a better explanation.
+
 The build script will automatically copy the `VERSION` into a constant under `utils.AppVersion`.
--- build.sh
+++ build.sh
@@ -4,12 +4,14 @@ FULL_NAME="github.com/callumj/example"
 APP_NAME="example"
 RESOURCES="utils app main.go"
 
+# Find the current branch
 if [[ -z "$BUILDBOX_BRANCH" ]]
 then
   BUILDBOX_BRANCH=`git branch | sed -n '/\* /s///p'`
 fi
 VERSION=`cat VERSION`
 
+# Sanity check on the branch
 if ! [[ "${BUILDBOX_BRANCH}" == "master" ]]
 then
   if [[ "${BUILDBOX_BRANCH}" == "development" ]]
@@ -21,7 +23,9 @@ then
   fi
 fi
 
+# Start with a fresh Golang environment everytime
 rm -r -f tmp/go
+# Remove any existing of the same version
 rm -r -f builds/${VERSION}
 
 # vet the source (capture errors because the current version does not use exit statuses currently)
@@ -33,22 +37,30 @@ cur=`pwd`
 if ! [ -n "$VET" ]
 then
   echo "All good"
+  # Prep
   mkdir -p tmp/go
   mkdir -p builds/
   mkdir tmp/go/src tmp/go/bin tmp/go/pkg
   mkdir -p tmp/go/src/${FULL_NAME}
+
+  # Copy our golang sources into the temp go path
   cp -R ${RESOURCES} tmp/go/src/${FULL_NAME}
 
+  # Bundle in our version constant
   go_src=$'package utils\nconst AppVersion string = "'"${VERSION}"$'"'
   echo "$go_src" > tmp/go/src/${FULL_NAME}/utils/version.go
 
+  # allocate support for the builds
   mkdir -p builds/${VERSION}/darwin_386 builds/${VERSION}/darwin_amd64 builds/${VERSION}/linux_386 builds/${VERSION}/linux_amd64
   mkdir -p builds/${VERSION}/windows_386 builds/${VERSION}/windows_amd64
+
+  # fake our Go environment and force the Golang to resolve
   GOPATH="${cur}/tmp/go"
   echo "Getting"
   GOPATH="${cur}/tmp/go" go get -d .
   echo "Starting build"
 
+  # cross compile
   GOPATH="${cur}/tmp/go" GOOS=darwin GOARCH=386 go build -o builds/${VERSION}/darwin_386/${APP_NAME}
 
   GOPATH="${cur}/tmp/go" GOARCH=amd64 GOOS=darwin go build -o builds/${VERSION}/darwin_amd64/${APP_NAME}
@@ -65,7 +77,7 @@ else
   exit -1
 fi
 
-# rewrite the binaries
+# move the binaries into tar-gz archives
 
 FILES=builds/${VERSION}/*/${APP_NAME}
 for f in $FILES
